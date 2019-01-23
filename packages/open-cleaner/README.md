e8d326f2426425093f039f28557c016931f62aa7
Moritz Brandes <mba@ashampoo-systems.com>
2019-01-23 19:16:59 +0100

    Added Electron app

---
+++ packages/open-cleaner/README.md
@@ -0,0 +1,14 @@
+## Usage
+
+```bash
+$ cd my-app
+
+# Install dependencies
+$ yarn (or `npm install`)
+
+# Run development mode
+$ yarn dev (or `npm run dev`)
+
+# Build packages
+$ yarn build (or `npm run build`)
+```
---
+++ packages/open-cleaner/renderer/pages/about.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { withStyles, WithStyles, Theme, createStyles } from '@material-ui/core/styles'
+import { resolve } from '../helpers'
+
+const styles = ({ spacing }: Theme) => createStyles({
+  root: {
+    textAlign: 'center',
+    paddingTop: spacing.unit * 4
+  }
+})
+
+interface Props extends WithStyles<typeof styles> {}
+
+const About = withStyles(styles)(
+  class extends React.Component<Props> {
+    render() {
+      const { classes } = this.props
+
+      return (
+        <div className={classes.root}>
+          <Typography variant="h4" gutterBottom>
+            Material-UI
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            with Nextron
+          </Typography>
+          <Typography gutterBottom>
+            <a href={resolve('home')}>
+              Go to the home page
+            </a>
+          </Typography>
+          <Button variant="contained" color="primary">
+            Do nothing button
+          </Button>
+        </div>
+      )
+    }
+  }
+)
+
+export default About
