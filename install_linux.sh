cebe80f524f76fcefa15be36da2a267e3254920c
Moritz Brandes <mba@ashampoo-systems.com>
2019-01-26 16:49:41 +0100

    Added reload bash to script

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,6 @@
 echo "Installing Ubuntu dependencies"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+exec bash
 nvm install node
 echo "Running on node version"
 node --version
