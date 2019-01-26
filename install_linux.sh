c1a18de0284fa81c5929078be248fa4c3d700ff7
Moritz Brandes <mba@ashampoo-systems.com>
2019-01-26 16:04:47 +0100

    Added nvm for linux ci

--- install_linux.sh
+++ install_linux.sh
@@ -1,6 +1,5 @@
 echo "Installing Ubuntu dependencies"
-curl -sL https://deb.nodesource.com/setup_11.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt-get install -y nodejs
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+nvm install node
 echo "Running on node version"
 node --version
