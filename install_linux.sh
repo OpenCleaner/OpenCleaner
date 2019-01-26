81010e6c604c83a0604751320fc9a261b57255c2
Moritz Brandes <mba@ashampoo-systems.com>
2019-01-26 16:33:24 +0100

    Changed line endings

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,5 @@
 echo "Installing Ubuntu dependencies"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o nvm_install.sh
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 sudo bash nvm_install.sh
 nvm install node
 echo "Running on node version"
